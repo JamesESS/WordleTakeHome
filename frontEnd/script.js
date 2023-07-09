55e5ec3b7770ea5b84ea17df73dab8a848e35b3b
JamesESS <60142087+JamesESS@users.noreply.github.com>
2023-07-09 22:26:10 +0100

    added POST method to update word

--- frontEnd/script.js
+++ frontEnd/script.js
@@ -1,15 +1,24 @@
-fetch("https://qgxxfd5hoh.execute-api.eu-west-2.amazonaws.com/DEV/getword")
+const wordURL = "https://qgxxfd5hoh.execute-api.eu-west-2.amazonaws.com/DEV/getword";
+fetch(wordURL)
 .then(response => response.json())
 .then(result => {
     let todaysWord = result.Items[0].word.S;
-  console.log(result.Items[0].word.S);
-  document.getElementById("wordOutput").textContent = result.Items[0].word.S;
-  let wordArr = todaysWord.split("");
-  console.log(wordArr);
-  if (wordArr.length != 5) console.error("incorrect word length!");
-  wordArr.forEach(letter => {
+    let wordArr = todaysWord.split("");
+    console.log(wordArr);
+    if (wordArr.length != 5) console.error("incorrect word length!");
+    wordArr.forEach(letter => {
         const letterContainer = document.createElement("p");
         letterContainer.textContent = letter;
+        letterContainer.classList.add("letter");
         document.getElementById("wordOutput").appendChild(letterContainer);
   });
 })
+
+
+const updateWord = () => {
+    fetch(wordURL, {
+        method: "POST"
+    })
+}
+
+document.getElementById("updateWord").addEventListener("click",updateWord)
