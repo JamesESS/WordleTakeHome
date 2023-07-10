ed1750f0d04f34c6d121277daeed8cc9b736779d
JamesESS <60142087+JamesESS@users.noreply.github.com>
2023-07-10 17:01:06 +0100

    code reformat

--- frontEnd/script.js
+++ frontEnd/script.js
@@ -1,51 +1,61 @@
 const board = document.createElement("div");
-board.className = "playBoard"
-board.id = "playBoard"
-makeRow(0,"DIV","row",board);
+board.className = "playBoard";
+board.id = "playBoard";
+makeRow(0, "div", "row", board);
 document.getElementById("boardContainer").appendChild(board);
 
-function makeRow(i,tagName,className,parent) {
-    if (i<7) {
-        const row = document.createElement(tagName);
-        row.className = className;
-        parent.appendChild(row);
-        makeTile(0,"P","tile",row);
-        makeRow(i+1,tagName,className,parent)
-    }
-    else return
+function makeRow(i, tagName, className, parent) {
+  if (i < 7) {
+    const row = document.createElement(tagName);
+    row.className = className;
+    parent.appendChild(row);
+    makeTile(0, "p", "tile", row);
+    makeRow(i + 1, tagName, className, parent);
+  }
 }
 
-function makeTile(i,tagName,className,parent) {
-    if (i<5) {
-        const row = document.createElement(tagName);
-        row.className = className;
-        parent.appendChild(row);
-        makeTile(i+1,tagName,className,parent)
-    }
-    else return
+function makeTile(i, tagName, className, parent) {
+  if (i < 5) {
+    const tile = document.createElement(tagName);
+    tile.className = className;
+    parent.appendChild(tile);
+    makeTile(i + 1, tagName, className, parent);
+  }
 }
 
 const wordURL = "https://qgxxfd5hoh.execute-api.eu-west-2.amazonaws.com/DEV/getword";
 
 const getWord = () => {
-    fetch(wordURL)
+  fetch(wordURL)
     .then(response => response.json())
     .then(result => {
-        let todaysWord = result.Items[0].word.S;
-        let wordArr = todaysWord.split("");
-        if (wordArr.length != 5) console.error("incorrect word length!");
-        wordArr.forEach((letter,i) => {
-            document.getElementById("playBoard").childNodes[6].childNodes[i].textContent = letter;
+      const todaysWord = result.Items[0].word.S;
+      const wordArr = todaysWord.split("");
+      if (wordArr.length !== 5) {
+        console.error("Incorrect word length!");
+      }
+      wordArr.forEach((letter, i) => {
+        const playBoard = document.getElementById("playBoard");
+        const row = playBoard.childNodes[6];
+        const tile = row.childNodes[i];
+        tile.textContent = letter;
       });
     })
-}
+    .catch(error => {
+      console.error("Failed to fetch word:", error);
+    });
+};
+
 getWord();
 
 const updateWord = () => {
-    fetch(wordURL, {
-        method: "POST"
-    })
+  fetch(wordURL, {
+    method: "POST"
+  })
     .then(() => getWord())
-}
+    .catch(error => {
+      console.error("Failed to update word:", error);
+    });
+};
 
-document.getElementById("updateWord").addEventListener("click",updateWord)
+document.getElementById("updateWord").addEventListener("click", updateWord);
